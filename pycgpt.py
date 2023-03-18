066b45cfb5ae6196010df6a07e72bf8b15e9a286
Aryan <93074700+useraid@users.noreply.github.com>
2023-03-18 12:42:48 +0530

    Command line input added

--- pycgpt.py
+++ pycgpt.py
@@ -1,14 +1,22 @@
 import requests
 from dotenv import load_dotenv
 import os
+import argparse
+import datetime
 
 def secret():
     load_dotenv()
 
 secret()
 
+parser = argparse.ArgumentParser()
+parser.add_argument("prompt", help="Code needed")
+args = parser.parse_args()
+
 openai_api_ep = "https://api.openai.com/v1/completions"
 api_key = os.getenv('api_key')
+
+timeStamp = "\n" + str(datetime.datetime.now())
 
 postHeader = {
     "Content-Type": "application/json",
@@ -17,7 +25,7 @@ postHeader = {
 
 postData = {
     "model": "text-davinci-003",
-    "prompt": "Write a paragraph on how cats are better than dogs",
+    "prompt": f"{args.prompt}",
     "max_tokens": 200,
     "temperature": 0.5
 }
@@ -25,6 +33,11 @@ postData = {
 response = requests.post(openai_api_ep, headers=postHeader, json=postData)
 
 if response.status_code == 200:
-    print(response.json())
+    output = response.json()["choices"][0]["text"]
+    print(output)
+    with open("pycgpt.log", "a") as file:
+        file.write(timeStamp)
+        file.write(output)
+
 else:
     print(f"Malformed/Invalid Response \nStatus Code: {str(response.status_code)}")
